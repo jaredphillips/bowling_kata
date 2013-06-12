efbbbfa6a5d17aa57a6ebd35bb4c3dba76b32e5f
jaredphillips <jaredtphillips@me.com>
2013-06-12 09:54:16 -0400

    Test for returning a gutter game passes

--- game.rb
+++ game.rb
@@ -1,7 +1,17 @@
 
 class Game
 
+	attr_accessor :pins
+
 	def roll(pins)
+		pins
 	end
-	
+
+	def pins
+	end
+
+	def score
+		0
+	end
+
 end
--- game_test.rb
+++ game_test.rb
@@ -11,7 +11,6 @@ class GameTest < Test::Unit::TestCase
 
 	def test_should_be_able_to_roll_ball
 		game = Game.new
-		puts game.roll(pins)
 		assert game.roll(0)
 	end
 
@@ -21,6 +20,14 @@ class GameTest < Test::Unit::TestCase
 			game.roll(0)
 		end
 		assert_equal game.score, 0
+	end
+
+	def test_it_should_return_20_for_a_game_of_all_ones
+		game = Game.new
+		20.times do
+			game.roll(20)
+		end
+		assert_equal game.score, 20
 	end
 
 
